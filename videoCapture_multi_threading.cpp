36aaa26b458dcd84f7c8be028b727cabadf39601
康洋 <370846270@qq.com>
2016-09-13 15:03:27 +0800

    Create videoCapture_multi_threading.cpp

---
+++ videoCapture_multi_threading.cpp
@@ -0,0 +1,34 @@
+#include <boost/thread.hpp>
+#include <opencv2/opencv.hpp>
+using std::string;
+void fun(string path){
+    cv::VideoCapture cap(path);
+    if(cap.isOpened()){
+        while(true){
+            cv::Mat frame;
+            cap.read(frame);
+            cv::Mat resized_frame;
+            cv::resize(frame, resized_frame, cv::Size(100,100));
+            if(frame.empty())
+                break;
+            
+        }
+    }
+    cap.release();
+}
+int main(){
+    boost::thread trd1(fun, "/Users/zj-db0655/Documents/data/528100078_5768b1b1764438418.mp4");
+    boost::thread trd2(fun, "/Users/zj-db0655/Documents/data/528100078_5768b1b1764438418.mp4");
+    boost::thread trd3(fun, "/Users/zj-db0655/Documents/data/528100078_5768b1b1764438418.mp4");
+    boost::thread trd4(fun, "/Users/zj-db0655/Documents/data/528100078_5768b1b1764438418.mp4");
+    boost::thread trd5(fun, "/Users/zj-db0655/Documents/data/528100078_5768b1b1764438418.mp4");
+    boost::thread trd6(fun, "/Users/zj-db0655/Documents/data/528100078_5768b1b1764438418.mp4");
+    trd1.join();
+    trd2.join();
+    trd3.join();
+    trd4.join();
+    trd5.join();
+    trd6.join();
+
+    return 0;
+}
